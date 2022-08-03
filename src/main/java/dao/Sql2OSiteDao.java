78e56d8bfcf80561127398692265e4bb5d192b18
jtaraya <jacobtaraya@gmail.com>
2022-08-03 14:02:59 +0300

    Sql2o aobject call method

--- src/main/java/dao/Sql2OSiteDao.java
+++ src/main/java/dao/Sql2OSiteDao.java
@@ -1,7 +1,9 @@
 package dao;
 
 import models.Site;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -9,12 +11,28 @@ public class Sql2OSiteDao implements SiteDao {
 
     private final Sql2o sql2o;
     public Sql2OSiteDao(Sql2o sql2o){
-        this.sql2o = sql2o; //making the sql2o object available everywhere so we can call methods in it
+        this.sql2o = sql2o;
+        //making the sql2o object available everywhere so we can call methods in it
     }
 
     @Override
     public List<Site> getAll() {
         return null;
+    }
+
+    @Override
+    public void add(Site site) {
+        String sql = "INSERT INTO sites (siteName, siteId, engineerName)" + "VALUES (:siteName, :siteId, :engineerName);";
+        try(Connection con = DB.sql2o.open()){ //try to open a connection
+            int id = (int) con.createQuery(sql, true) //make a new variable
+                    .bind(site)
+                    .executeUpdate()
+                    .getKey();
+            site.setId(id); //update object to set id now from database
+            System.out.println(sql);
+        } catch (Sql2oException ex) {
+            System.out.println();
+        }
     }
 
     @Override
