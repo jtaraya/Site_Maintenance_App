8d13952b0a5156bcda80413a7521192d32f4077a
jtaraya <jacobtaraya@gmail.com>
2022-08-03 12:37:20 +0300

    added engineer method

--- src/main/java/models/Engineer.java
+++ src/main/java/models/Engineer.java
@@ -1,6 +1,8 @@
 package models;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class Engineer {
     private int id;
@@ -11,7 +13,7 @@ public class Engineer {
     private String engineerDetails;
     private ArrayList<Site> sites;
     private int mobileNo;
-}
+
 
     public int getId() {
         return id;
@@ -57,3 +59,35 @@ public class Engineer {
     public void setMobileNo(int mobileNo) {
         this.mobileNo = mobileNo;
     }
+
+    //    public static List<Engineer> all() {
+//        String sql = "SELECT id, name FROM engineers";
+//        try(Connection con = DB.sql2o.open()){
+//            return con.createQuery(sql).executeAndFetch(Engineer.class);
+//        }
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Engineer engineer = (Engineer) o;
+        return getId() == engineer.getId() &&
+                Objects.equals(getName(), engineer.getName()) &&
+                Objects.equals(getEmp_no(), engineer.getEmp_no()) &&
+                Objects.equals(getMobileNo(), engineer.getMobileNo()) &&
+                Objects.equals(sites, engineer.sites);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getId(), getEmp_no(), getMobileNo(), sites);
+    }
+
+}
+
+
+
+//
+//
