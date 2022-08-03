d58bbb27c51e1f187eb412a73d84f5df86b6fb05
jtaraya <jacobtaraya@gmail.com>
2022-08-03 14:53:57 +0300

    edit DB class

---
+++ src/main/java/dao/Constants.java
@@ -0,0 +1,8 @@
+package dao;
+
+public class Constants {
+    public static final String USERNAME="postgres";
+    public static final String PASSWORD="postgres";
+    public static final String URL_TEST="postgres://localhost:5432/site_manager_test";
+    public static final String URL_PROD="postgres://localhost:5432/site_manager";
+}
