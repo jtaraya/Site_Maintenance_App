0c643b45edd2ee392b2cefa84815e888403a2ae1
jtaraya <jacobtaraya@gmail.com>
2022-08-03 22:01:46 +0300

    get request and response

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -3,8 +3,14 @@ package models;
 import dao.DB;
 import dao.Sql2OEngineerDao;
 import dao.Sql2OSiteDao;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import static spark.Spark.staticFileLocation;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
@@ -23,6 +29,15 @@ public class App {
             port = 4563;
         }
         port(port);
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Engineer> allEngineers = engineerDao.getAll();
+            model.put("engineers", allEngineers);
+            List<Site> sites = siteDao.getAll();
+            model.put("engineers", allEngineers);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
