c0cc4c783610dfdabec12d96cd6638f8e5da27a8
jtaraya <jacobtaraya@gmail.com>
2022-08-03 11:45:11 +0300

    added engineers

--- src/main/java/models/Engineer.java
+++ src/main/java/models/Engineer.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.time.LocalDateTime;
+
 public class Engineer {
+    private int id;
+    private int emp_no;
+    private String name;
+    private String siteAllocation;
+    private LocalDateTime hireDate;
+    private String engineerDetails;
+    private ArrayList<Site> sites;
+    private int mobileNo;
 }
